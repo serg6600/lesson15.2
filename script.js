0e0b5d57f7c1d8a7e6f8a2111a718f8818152cbb
Сергей Рожко <serg6600@yandex.ru>
2021-05-13 19:55:45 +0300

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class First {
+    constructor(){
+    }
+    hello(){
+        console.log("Привет я метод родителя!");
+    }
+}
+
+class Second extends First{
+    constructor(){
+        super();
+    }
+    hello(){
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
+
+const first = new First();
+first.hello();
+const second = new Second();
+second.hello();
